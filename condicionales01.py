d4c220d4a3c11868b2a64d924dbf3c7b14f86de8
GerardoRG136 <145025998+GerardoRG136@users.noreply.github.com>
2023-09-27 21:47:42 -0600

    Condicionales01.py

---
+++ condicionales01.py
@@ -0,0 +1,33 @@
+temp = float(input("Temperatura:"))
+if temp > 90:
+  print("¡Qué calor!")
+
+número = float(input("Ingresa un número:"))
+if número > 0:
+  print("Tu numero es positivo")
+elif número < 0:
+  print("Tu numero es negativo")
+else:
+  print("Tu numero es cero")
+
+
+num = float(input("Ingresa un número:"))
+if -10<=num<=10:
+  print("Hurra")
+else:
+  print("Soporta")
+
+respuesta = input("Camarón que se duerme: ")
+print("A. se lo lleva la corriente")
+print("B. jamas se endereza")
+print ("Escribe tu respuesta: ")
+if respuesta == input("A"):
+  print("Correcto!")
+elif respuesta == input("B"):
+  print("No es correcto")
+
+x = int(input("dame un numero"))
+if x >= 0:
+  print("x es positivo")
+else:
+  print("x no es positivo")
